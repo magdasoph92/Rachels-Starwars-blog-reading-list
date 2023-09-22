8bae5920b777f1f4054d7d2cb04ab5d8c6e5d31f
Rachel Sophia <137792219+magdasoph92@users.noreply.github.com>
2023-09-22 17:56:34 +0000

    Rachel Starwars Blog Reading List

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-3 text-center text-light">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			<a href="https://github.com/magdasoph92" target="_blank">Rachel Sophia aka magdasoph92</a>
 		</p>
 	</footer>
 );
